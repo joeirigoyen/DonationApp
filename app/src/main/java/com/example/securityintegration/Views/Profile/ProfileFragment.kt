2c8ca13517a9d5d86edcc6543598f83dc5b73d39
Youthan Irigoyen <a01750476@itesm.mx>
2021-10-07 02:50:40 -0500

    Readded profile fragment

--- app/src/main/java/com/example/securityintegration/Views/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/securityintegration/Views/Profile/ProfileFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.securityintegration.R
 import com.example.securityintegration.ViewModels.Profile.ProfileViewModel
+import com.example.securityintegration.databinding.ProfileFragmentBinding
 
 class ProfileFragment : Fragment() {
 
@@ -15,19 +17,14 @@ class ProfileFragment : Fragment() {
         fun newInstance() = ProfileFragment()
     }
 
-    private lateinit var viewModel: ProfileViewModel
+    private lateinit var binding : ProfileFragmentBinding
+    private val viewModel : ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+    ): View {
+        binding = ProfileFragmentBinding.inflate(layoutInflater)
+        return binding.root
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
